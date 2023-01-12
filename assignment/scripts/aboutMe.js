830652a8f4196d724d981ae2493cba26b3ca1368
Corey <corey@mk-gang-imac.lan>
2023-01-12 16:36:59 -0500

    completed problem 18

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -29,12 +29,9 @@ pets += 2;
 const allowedPets = 4
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-if( adventurous ){
-console.log('Adventures are great!')
-}
-else{
+adventurous ?
+console.log('Adventures are great!') :
     console.log('How about we stay home?')
-}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
